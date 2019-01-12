ea541b7e1fa13e585602e99064dbfc3598d7b057
AlejandraCP <alejandracp710@gmail.com>
2019-01-12 13:24:50 -0500

    Link in next

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Home = (props) => {
+  return (
+    <div>
+      <p>Home</p>
+      <Link href='/sell'>
+        Sell
+      </Link>
+    </div>
+  );
+}
+
+export default Home;  
